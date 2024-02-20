f2bf610617fe3fa5c561aca35d5546b607a2ace5
MuhammadRajaFadhilHabibi <126538837+MuhammadRajaFadhilHabibi@users.noreply.github.com>
2024-02-20 11:32:26 +0700

    Add files via upload

---
+++ Petemuan 1/Titik.java
@@ -0,0 +1,40 @@
+class Titik{
+    //Deklarasi atribut
+    double absis;
+    double ordinat;
+    static int counterTitik;
+
+    //Deklarasi konstruktor
+    Titik(){
+        absis = 0;
+        ordinat = 0;
+        counterTitik++;
+    }
+
+    Titik(double a, double b){
+        absis = a;
+        ordinat = b;
+        counterTitik++;
+        }
+
+    //Deklarasi metode
+    void setAbsis(double a){
+        absis = a;
+    }
+
+    void setOrdinat(double o){
+        ordinat = o;
+    }
+
+    double getAbsis(){
+        return absis;
+    }
+
+    double getOrdinat(){
+        return ordinat;
+    }
+
+    int getCounterTitik(){
+        return counterTitik;
+    }
+}
