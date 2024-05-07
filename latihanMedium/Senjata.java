b76de016fbf4fd6fdee937093fcce9b5b707a098
MuhammadRajaFadhilHabibi <126538837+MuhammadRajaFadhilHabibi@users.noreply.github.com>
2024-05-07 11:47:31 +0700

    Add files via upload

---
+++ latihanMedium/Senjata.java
@@ -0,0 +1,41 @@
+/*Nama: Muhammad Raja Fadhil Habibi
+ *NIM: 24060122140131
+  PRAKTIKUM PBO - LatihanMedium */
+
+package latihanMedium;
+
+public class Senjata{
+    private String bunyi;
+    private int peluru;
+    private boolean menusuk;
+
+    public Senjata(String bunyi){
+        this.bunyi = bunyi;
+        this.menusuk = false;
+        this.peluru = 0;
+    }
+
+    public String getBunyi(){
+        return this.bunyi;
+    }
+
+    public int getPeluru(){
+        return this.peluru;
+    }
+
+    public boolean isMenusuk(){
+        return this.menusuk;
+    }
+
+    public void setBunyi(String bunyi){
+        this.bunyi = bunyi;
+    }
+
+    public void setMenusuk(boolean menusuk){
+        this.menusuk = menusuk;
+    }
+
+    public void setPeluru(int peluru){
+        this.peluru = peluru;
+    }
+}
